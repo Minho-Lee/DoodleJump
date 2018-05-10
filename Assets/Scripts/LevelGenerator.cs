3bd497960522aa726a5967c868386eb3152f49cf
Minho Lee <minho.lee.93@hotmail.com>
2018-05-10 17:05:04 -0400

    WhitePlatforms (disappearing ones) are now in place

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
 
+	public static LevelGenerator LG;
+
 	public GameObject platformPrefab;
 	public GameObject bluePlatformPrefab;
+	public GameObject whitePlatformPrefab;
 
 	public int numOfPlatforms = 200;
 	public float levelWidth = 3f;
 	public float minY = .3f;
 	public float maxY = 1.2f;
 
+
+	void Awake()
+	{
+		if (LG == null)
+		{
+			LG = this;
+		}
+	}
 	// Use this for initialization
 	void Start () {
 		Vector3 spawnPosition = new Vector3();
@@ -21,12 +32,12 @@ public class LevelGenerator : MonoBehaviour {
 			spawnPosition.x = Random.Range (-levelWidth, levelWidth);
 			spawnPosition.y += Random.Range (minY, maxY);
 			chance = Random.Range (1, 100);
-			if (chance < 80) {
+			if (chance < 85) {
 				Instantiate (platformPrefab, spawnPosition, Quaternion.identity);
-			} 
-			else
-			{
+			} else if (chance < 95) {
 				Instantiate (bluePlatformPrefab, spawnPosition, Quaternion.identity);
+			} else {
+				Instantiate (whitePlatformPrefab, spawnPosition, Quaternion.identity);
 			}
 		}	 
 	}
--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour {
 
-	public float jumpForce = 11f;
+	public float jumpForce;
+
+	void Awake()
+	{
+		if (this.tag == "Platform") {
+			jumpForce = 11f;
+		} else if (this.tag == "BluePlatform") {
+			jumpForce = 15f;
+		} else {
+			jumpForce = 0f;
+		}		
+	}
 
 	void OnCollisionEnter2D(Collision2D collision) 
 	{
@@ -12,16 +23,44 @@ public class Platform : MonoBehaviour {
 		if (collision.relativeVelocity.y <= 0f) 
 		{
 			Rigidbody2D rb = collision.collider.GetComponent <Rigidbody2D>();
-			Debug.Log (collision.collider.name);
-			Debug.Log (rb);
+			//Debug.Log (collision.collider.name);
 			if (rb != null)
 			{
 				Vector2 velocity = rb.velocity;
 				velocity.y = jumpForce;
 				rb.velocity = velocity;
 			}	
-		}
+			// Make WhitePlatform disappear and disable collision as soon as it bumps
 
+			if (this.name.Contains ("BluePlatform"))
+			{
+				Debug.Log ("BLUE PLATFORM");
+			}
+			else if (this.name.Contains ("Platform"))
+			{
+				Debug.Log ("PLATFORM");
+			}
+		} 
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Rigidbody2D rb = collider.GetComponent <Rigidbody2D>();
+		//Debug.Log (rb.velocity.y);
+		if (rb.velocity.y <= 0)
+		{
+			//this.GetComponent <EdgeCollider2D>().enabled = false;
+			//Debug.Log ("WHITE PLATFORM");
+			StartCoroutine ("DisappearingAnimation");
+		}
+	}
+
+	private IEnumerator DisappearingAnimation()
+	{
+		Animator animator = transform.GetComponent <Animator>();
+		animator.enabled = true;
+		yield return new WaitForSeconds (1f);
+		Destroy (this.gameObject);
 	}
 
 }
